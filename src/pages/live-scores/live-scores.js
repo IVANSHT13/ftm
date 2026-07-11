7403a77f19ed9e341b97872da3bde0776998eb78
IVANSHT13 <i.g.shterev@gmail.com>
2026-07-12 00:28:10 +0300

    livescore

---
+++ src/pages/live-scores/live-scores.js
@@ -0,0 +1,8 @@
+import liveScoresTemplate from './live-scores.html?raw';
+import './live-scores.css';
+
+export const title = 'Pro League | Live Scores';
+
+export function render() {
+  return liveScoresTemplate;
+}
